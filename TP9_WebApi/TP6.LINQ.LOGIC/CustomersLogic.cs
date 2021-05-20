53bc01eacb29fa9fdbe610344b691fe00cb0ae4d
Torres Ignacio <Torres.Ignacio.TI@gmail.com>
2021-05-20 15:29:31 -0300

    Se agrega try catch en todos los metodos de la logica que llamen a la funcion SaveChanges(). Se corrige bug que no dejaba crear usuario nuevo con contactName vacio

--- TP9_WebApi/TP6.LINQ.LOGIC/CustomersLogic.cs
+++ TP9_WebApi/TP6.LINQ.LOGIC/CustomersLogic.cs
@@ -100,74 +100,100 @@ namespace TP6.LINQ.LOGIC
 
         public bool Add(Customers newCustomer)
         {
-            //Verifica que la id del nuevo cliente que se quiere agregar no exista
-            ValidateCustomer(newCustomer);
-
-            if (!context.Customers.Any(c => c.CustomerID == newCustomer.CustomerID))
+            try
             {
-                context.Customers.Add(newCustomer);
-                context.SaveChanges();
-                return true;
-            }
-            return false;
+                //Verifica que la id del nuevo cliente que se quiere agregar no exista
+                ValidateCustomer(newCustomer);
 
+                if (!context.Customers.Any(c => c.CustomerID == newCustomer.CustomerID))
+                {
+                    context.Customers.Add(newCustomer);
+                    context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }   
         }
 
         public void Delete(string id)
         {
-            if (id == null)
-                throw new ArgumentException("La ID no puede estar vacía");
-            if (id.Length != 5)
-                throw new ArgumentException("La ID debe tener 5 letras");
-            if (!id.All(c => char.IsLetter(c)))
-                throw new ArgumentException("La ID no puede contener números o símbolos");
+            try
+            {
+                if (id == null)
+                    throw new ArgumentException("La ID no puede estar vacía");
+                if (id.Length != 5)
+                    throw new ArgumentException("La ID debe tener 5 letras");
+                if (!id.All(c => char.IsLetter(c)))
+                    throw new ArgumentException("La ID no puede contener números o símbolos");
 
-            var customerAEliminar = context.Customers.Find(id);
-            if (customerAEliminar == null)
-                throw new ArgumentNullException("No existe ningún customer con esa ID");
-            context.Customers.Remove(customerAEliminar);
-            context.SaveChanges();
-            
+                var customerAEliminar = context.Customers.Find(id);
+                if (customerAEliminar == null)
+                    throw new ArgumentNullException("No existe ningún customer con esa ID");
+                context.Customers.Remove(customerAEliminar);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
 
         //UpdateLazy updatea los campos provistos dejando tal cual estaban los que no han sido especificados
         public bool UpdateLazy(Customers customer)
-         {
-            var customerUpdate = context.Customers.Find(customer.CustomerID);
-            if (customerUpdate == null)
-                return false;
-            ValidateCustomer(customer);
-            
+        {
+            try
+            {
+                var customerUpdate = context.Customers.Find(customer.CustomerID);
+                if (customerUpdate == null)
+                    return false;
+                ValidateCustomer(customer);
 
-            customerUpdate.Address              = customer.Address              ?? customerUpdate.Address;
-            customerUpdate.City                 = customer.City                 ?? customerUpdate.City;
-            customerUpdate.CompanyName          = customer.CompanyName          ?? customerUpdate.CompanyName;
-            customerUpdate.ContactName          = customer.ContactName          ?? customerUpdate.ContactName;
-            customerUpdate.ContactTitle         = customer.ContactTitle         ?? customerUpdate.ContactTitle;
-            customerUpdate.Country              = customer.Country              ?? customerUpdate.Country;
 
-            context.SaveChanges();
-            return true;
+                customerUpdate.Address = customer.Address ?? customerUpdate.Address;
+                customerUpdate.City = customer.City ?? customerUpdate.City;
+                customerUpdate.CompanyName = customer.CompanyName ?? customerUpdate.CompanyName;
+                customerUpdate.ContactName = customer.ContactName ?? customerUpdate.ContactName;
+                customerUpdate.ContactTitle = customer.ContactTitle ?? customerUpdate.ContactTitle;
+                customerUpdate.Country = customer.Country ?? customerUpdate.Country;
+
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         //UpdateAndBlank updatea los campos provistos y blanquea los que no han sido especificados
         public bool UpdateAndBlank(Customers customer) ///Devuelve un null si no encuentra nada
         {
-            var customerUpdate = context.Customers.Find(customer.CustomerID);
-            if (customerUpdate == null)
-                return false;
-            ValidateCustomer(customer);
-            
-            customerUpdate.Address              = customer.Address;
-            customerUpdate.City                 = customer.City;
-            customerUpdate.CompanyName          = customer.CompanyName;
-            customerUpdate.ContactName          = customer.ContactName;
-            customerUpdate.ContactTitle         = customer.ContactTitle;
-            customerUpdate.Country              = customer.Country;
-            
-            context.SaveChanges();
-            return true;
+            try
+            {
+                var customerUpdate = context.Customers.Find(customer.CustomerID);
+                if (customerUpdate == null)
+                    return false;
+                ValidateCustomer(customer);
+
+                customerUpdate.Address = customer.Address;
+                customerUpdate.City = customer.City;
+                customerUpdate.CompanyName = customer.CompanyName;
+                customerUpdate.ContactName = customer.ContactName;
+                customerUpdate.ContactTitle = customer.ContactTitle;
+                customerUpdate.Country = customer.Country;
+
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public int CalcPad(List<Customers> customers)
@@ -180,19 +206,21 @@ namespace TP6.LINQ.LOGIC
         {
             if (customer.CustomerID == null)
                 throw new ArgumentException("La ID no puede estar vacía");
-            if (customer.CompanyName == null)
-                throw new ArgumentException("El nombre de la compañía no puede estar vacío");
             if (customer.CustomerID.Length != 5)
                 throw new ArgumentException("La ID debe tener 5 letras");
             if (!customer.CustomerID.All(c => char.IsLetter(c)))
                 throw new ArgumentException("La ID no puede contener números o símbolos");
-            if (!customer.ContactName.All(c => char.IsLetter(c) || char.IsSeparator(c)))
-                throw new ArgumentException("ContactName no puede contener números o símbolos");
-
+            if (customer.CompanyName == null)
+                throw new ArgumentException("El nombre de la compañía no puede estar vacío");
             if (customer.CompanyName.Length > 40)
                 throw new ArgumentException("CompanyName no puede tener más de 40 caracteres");
-            if (customer.ContactName?.Length > 30)
-                throw new ArgumentException("ContactName no puede tener más de 30 caracteres");
+            if (customer.ContactName != null)
+            {
+                if (!customer.ContactName.All(c => char.IsLetter(c) || char.IsSeparator(c)))
+                    throw new ArgumentException("ContactName no puede contener números o símbolos");
+                if (customer.ContactName.Length > 30)
+                    throw new ArgumentException("ContactName no puede tener más de 30 caracteres");
+            }
             return;
         }
 
